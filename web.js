c618fb54726373e109c1d7320499b2743ab94023
Gerlito Chagas <Gerlito.Chagas@microchip.com>
2022-08-26 13:39:14 +0800

    only move forward

--- web.js
+++ web.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// const port = 3000
+
+// const thebody = {
+//   "_links": {
+//     "self": {
+//       "href": "https://YOUR_SERVICE_URL"
+//     }
+//   },
+//   "arena": {
+//     "dims": [4, 3], // width, height
+//     "state": {
+//       "https://A_PLAYERS_URL": {
+//         "x": 0, // zero-based x position, where 0 = left
+//         "y": 0, // zero-based y position, where 0 = top
+//         "direction": "N", // N = North, W = West, S = South, E = East
+//         "wasHit": false,
+//         "score": 0
+//       }
+
+//     }
+//   }
+// }
 
 app.use(bodyParser.json());
 
@@ -11,7 +33,7 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send(moves[0]);
 });
 
 app.listen(process.env.PORT || 8080);
